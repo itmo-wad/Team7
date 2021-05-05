296dcb86178fdf9d092ed035a6e5ddb4c4728dc9
Hashim <hashimdev1@gmail.com>
2021-05-06 01:45:04 +0300

    add url shortener

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, url_for
 from functools import wraps
 import pymongo
+from flask_sqlalchemy import SQLAlchemy
 import random
 import string
 import csv
@@ -8,6 +9,7 @@ import json
 
 #setting the flask app name as "app"
 app = Flask(__name__)
+#flask secret key (binary random string: print(os.ranodm) to manage sessions for users
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # Local Database connection
@@ -19,9 +21,50 @@ app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 client = pymongo.MongoClient("mongodb+srv://testuser:testpassword@cluster0.dtngb.gcp.mongodb.net/q4u?retryWrites=true&w=majority")
 db = client.test
 
+# Heroku SQLALCHEMY connection
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# Local SQLALCHEMY connection
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+sql = SQLAlchemy(app)
+
+@app.before_first_request
+def create_tables():
+    sql.create_all()
+
+
+class Urls(sql.Model):
+    id_ = sql.Column("id_", sql.Integer, primary_key=True)
+    long = sql.Column("long", sql.String())
+    short = sql.Column("short", sql.String(10))
+
+    def __init__(self, long, short):
+        self.long = long
+        self.short = short
+
+
+def shorten_url():
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters, k=3)
+        rand_letters = "".join(rand_letters)
+        short_url = Urls.query.filter_by(short=rand_letters).first()
+        if not short_url:
+            return rand_letters
+
+
+
+
+
+
 
 
 # Decorators: verify if the user currently logged in
+# *args: variable length arguments (1,2,3,4....etc)
+# variable length of keyword arguments (a:1,b:2,:c:3)
+# f: the route function as an argument
+#logged_in is the seesion value that is created earlier
 def login_required(f):
   @wraps(f)
   def wrap(*args, **kwargs):
@@ -29,7 +72,7 @@ def login_required(f):
       return f(*args, **kwargs)
     else:
       return redirect('/')
-  
+  # return the route function to direct the user to the destination page
   return wrap
 
 # Routes
@@ -58,15 +101,17 @@ def dashboard():
     # assign the first dictionary (the first row in the csv file) as the fieldnames for the table
     fieldnames = [key for key in results[0].keys()]
     # render the list of dictionaries to be viewd in the html table
-    return render_template('dashboard.html', results=results, fieldnames=fieldnames, len=len)
+    print(results)
+    return render_template('dashboard.html', results=results, fieldnames=fieldnames, len=len )
   # if there is no user input then render the page only
   elif request.method == 'GET':
     return render_template('dashboard.html')
 
 #route to transfer the sudent data that came from his/her clicked row and viewed privatly
 #the <row> argument is the string that contain the student data passd by the url_for in the html template
-@app.route('/info <row>')
+@app.route('/info <row>', methods=['GET', 'POST'])
 def info(row):
+
   #replase each single quotation (') mark with double quotation mark
   #because the dictionar must be enclosed with double quotation(") mark
   #the "\'" stands for the single quotation mark and the \ is an escape character so the
@@ -75,8 +120,45 @@ def info(row):
   str = row.replace("\'", "\"")
   # convert the double quoted string to a dictionary so the for loop in the info template can iterate it
   Diction = eval(str)
+
+  if request.method == "POST":
+    url_received = request.form["nm"]
+    found_url = Urls.query.filter_by(long=url_received).first()
+
+    if found_url:
+        return redirect(url_for("display_short_url", url=found_url.short))
+    else:
+        short_url = shorten_url()
+        print(short_url)
+        new_url = Urls(url_received, short_url)
+        sql.session.add(new_url)
+        sql.session.commit()
+        return redirect(url_for("display_short_url", url=short_url))
   #return the newly converted dictionary to the info page so it can be viewed as a table
   return render_template('info.html', rowDic = Diction)
 
 
+@app.route('/<short_url>')
+def redirection(short_url):
+    long_url = Urls.query.filter_by(short=short_url).first()
+    if long_url:
+        return redirect(long_url.long)
+    else:
+        return f'<h1>Url doesnt exist</h1>'
 
+
+@app.route('/display/<url>')
+def display_short_url(url):
+    return render_template('shorturl.html', short_url_display=url)
+
+
+
+
+
+
+
+
+
+@app.route('/all_urls')
+def display_all():
+    return render_template('all_urls.html', vals=Urls.query.all())
--- user/models.py
+++ user/models.py
@@ -5,11 +5,16 @@ import uuid
 
 # models to populate the mongodb collections
 class User:
-
+# self: access the attributes and methods of the class, 
+# It binds the attributes with the given arguments (user)
   def start_session(self, user):
+    # delete passowrd from the session value for security
     del user['password']
-    session['logged_in'] = True
+    # session is a built in flask function
+    session['logged_in'] = True #default vale true for the if condition usages
+    # user data is converted to a specific session value that belong to the user
     session['user'] = user
+    # return the user session with code 200 (success)
     return jsonify(user), 200
 
   def signup(self):
@@ -28,24 +33,29 @@ class User:
 
     # Check for existing email address
     if db.users.find_one({ "email": user['email'] }):
+      # if exist, return error with code 400
       return jsonify({ "error": "Email address already in use" }), 400
-
+    # if not exist, create new user and assign session
     if db.users.insert_one(user):
       return self.start_session(user)
-
+    # else for whatever reason, return error
     return jsonify({ "error": "Signup failed" }), 400
   
   def signout(self):
+    #clear the session
     session.clear()
+    #return to the main page (sign up form)
     return redirect('/')
   
   def login(self):
-
+    # query a user from db with the same email of the input form
     user = db.users.find_one({
+      #DB email == user input email
       "email": request.form.get('email')
     })
-
+    # check if the email and password of user input are the same in DB
     if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+      # if yes, start the session with theses values
       return self.start_session(user)
-    
+    # else return error
     return jsonify({ "error": "Invalid login credentials" }), 401
